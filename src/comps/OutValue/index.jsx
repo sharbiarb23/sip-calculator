530bd92d4712783a0dbf5278299d87d52825a87c
Achu Vyas <hcelab13@outlook.com>
2023-03-16 14:27:14 +0530

    float to int in out

--- src/comps/OutValue/index.jsx
+++ src/comps/OutValue/index.jsx
@@ -4,7 +4,7 @@ export default function OutValue(props) {
   const { label, value } = props;
   return (
     <div className="output-container">
-      {label} <div className="calc-amount">{value}$</div>
+      {label} <div className="calc-amount">{parseInt(value)}$</div>
     </div>
   );
 }
