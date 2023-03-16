42984b30bc99ad2acbd7453003c248af4f46e69b
achuvyas <iachuvyasvak@gmail.com>
2023-03-16 22:17:20 +0530

    ui updates & 3 extra calculations

--- src/App.jsx
+++ src/App.jsx
@@ -27,45 +27,22 @@ function App() {
       setterFn(e.target.value);
     }
   };
-  // const handleMonthlyInvestment = (e) => {
-  //   console.log(e.target.value, "the values");
-
-  //   if (e.target.value == "") {
-  //     setMonthlyInvestment(0);
-  //   } else if (e.target.value.length > 0 && e.target.value.charAt(0) == "0") {
-  //     setMonthlyInvestment(e.target.value.replace(/^0+(?=\d)/, ""));
-  //   } else if (e.target.value < 0) {
-  //     setMonthlyInvestment(1);
-  //   } else if (e.target.value > 100000) {
-  //     setMonthlyInvestment(100000);
-  //   } else {
-  //     setMonthlyInvestment(e.target.value);
-  //   }
-  // };
-  // const handleExpectedReturns = (e) => {
-  //   setExpectedReturns(e.target.value);
-  // };
-
-  // const handleTimePeriod = (e) => {
-  //   setTimePeriod(e.target.value);
-  // };
 
   useEffect(() => {
     let tax = expectedReturns * 0.04;
     setInvestedAmount(tax * (monthlyInvestment / 1000000000000));
-    // setEstReturns(
-    //   monthlyInvestment * timePeriod * 12 * (expectedReturns / 100)
-    // );
-
-    // let totalAmount =
-    //   monthlyInvestment * timePeriod * 12 +
-    //   monthlyInvestment * timePeriod * 12 * (expectedReturns / 100);
-    // setTotalValue(totalAmount);
   }, [monthlyInvestment, expectedReturns]);
 
   return (
     <>
-      <div className="header">SIP CALCULATOR</div>
+      <div className="header">
+        <img
+          src={"./public/dog.png"}
+          style={{ width: 80, height: 80 }}
+          alt="My Image"
+        />
+        <div style={{ marginLeft: 40 }}>SHARBI REWARDS CALCULATOR</div>
+      </div>
       <div className="container">
         <div className="calc-container">
           <div>
@@ -74,7 +51,7 @@ function App() {
               min={100000}
               max={20000000000}
               currentValue={monthlyInvestment}
-              showText={`  $`}
+              showText={`SHARBI`}
               onChange={handleInput}
               step={100000}
               setterFn={setMonthlyInvestment}
@@ -86,29 +63,19 @@ function App() {
               max={500000000}
               currentValue={expectedReturns}
               onChange={handleInput}
-              showText={`  $`}
+              showText={"$    "}
+              textAlign={true}
               step={50000}
               setterFn={setExpectedReturns}
             />
-
-            {/* <CustomSlider
-              label={"Time Period"}
-              min={1}
-              max={40}
-              currentValue={timePeriod}
-              onChange={handleInput}
-              showText={`yr`}
-              step={1}
-              setterFn={setTimePeriod}
-            /> */}
           </div>
+          <div className="rewards-title">Your reward in USD</div>
+
           <div className="output">
-            <OutValue
-              label="Your daily reward in USDC"
-              value={investedAmount}
-            />
-            {/* <OutValue label="Est returns" value={parseInt(estReturns)} />
-            <OutValue label="Total Value" value={totalValue} /> */}
+            <OutValue label="Daily" value={investedAmount} />
+            <OutValue label="Weekly" value={parseInt(investedAmount * 7)} />
+            <OutValue label="Monthly" value={parseInt(investedAmount * 30)} />
+            <OutValue label="Yearly" value={parseInt(investedAmount * 365)} />
           </div>
         </div>
       </div>
--- src/comps/CustomSlider/index.jsx
+++ src/comps/CustomSlider/index.jsx
@@ -1,30 +1,65 @@
 import { useState } from "react";
 
 const CustomSlider = (props) => {
-  const { label, min, max, currentValue, onChange, step, showText, setterFn } =
-    props;
+  const {
+    label,
+    min,
+    max,
+    currentValue,
+    onChange,
+    step,
+    showText,
+    setterFn,
+    textAlign,
+  } = props;
+
+  function convertAmount(amount) {
+    if (amount >= 1000000) {
+      return (amount / 1000000).toFixed(1) + "M";
+    } else if (amount >= 1000) {
+      return (amount / 1000).toFixed(1) + "K";
+    } else {
+      return amount.toString();
+    }
+  }
 
   return (
     <div className="slider-container">
       <div className="label-value">
         {label}{" "}
         <div className="input-container">
-          <input
-            className="input-box"
-            type={"number"}
-            value={currentValue}
-            onChange={(e) => {
-              onChange(e, max, setterFn);
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <input
+              className="input-box"
+              type={"number"}
+              value={currentValue}
+              onChange={(e) => {
+                onChange(e, max, setterFn);
+              }}
+              min={100}
+              max={10000}
+              step={1}
+            />
+            <div className="amount-short-title">
+              {" "}
+              {convertAmount(currentValue)}{" "}
+            </div>
+          </div>
+
+          <div
+            style={{
+              fontWeight: "bold",
+              height: 50,
+              fontSize: 15,
+              color: "#808080",
+              marginRight: textAlign ? 30 : 0,
             }}
-            min={100}
-            max={10000}
-            step={1}
-          />
-          <div style={{ fontWeight: "bold", fontSize: 20, color: "#808080" }}>
+          >
             {showText}
           </div>
         </div>
       </div>
+
       <input
         className="range-slider"
         style={{ width: "100%" }}
